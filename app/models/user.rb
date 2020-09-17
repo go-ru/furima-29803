94197bf2eecf7937965367f23e6e497883b0a8ae
go-ru <19970929+go-ru@users.noreply.github.com>
2020-09-17 21:14:20 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
   
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :furigana_first, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :furigana_last, format: { with: /\A[ァ-ヶー－]+\z/}
 
 end
