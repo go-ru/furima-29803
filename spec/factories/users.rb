51c849b5ee60a5b3bd61011e1855baf75410cefa
go-ru <19970929+go-ru@users.noreply.github.com>
2020-09-18 19:48:18 +0900

    パスワードの半角英数字に修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::JapaneseMedia::OnePiece.character }
     email                 {Faker::Internet.free_email }
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     first_name            {Gimei.last.kanji}
     last_name             {Gimei.first.kanji}
