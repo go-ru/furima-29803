18010f6598d52813301b1941c5b1a55c52e1714d
go-ru <19970929+go-ru@users.noreply.github.com>
2020-09-21 14:32:19 +0900

    itemモデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,5 +12,6 @@ class Item < ApplicationRecord
   validates :image, :name, :text, :category, :status, :delivery, :area, :day, :price, presence: true
 
   validates :category_id, :status_id, :delivery_id, :area_id, :day_id, numericality: { other_than: 0 }
-  # validates :price, format: { with: }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  
 end
