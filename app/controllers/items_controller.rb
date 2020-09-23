4c609e9a493e68b93d061fbd8707ebcfaae4d30b
go-ru <19970929+go-ru@users.noreply.github.com>
2020-09-23 12:25:45 +0900

    商品情報編集機能の実装-修正１回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  # before_action :authenticate_user! only: [:edit]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+   
   end
 
   def update
@@ -33,7 +33,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   private
@@ -44,5 +44,9 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
