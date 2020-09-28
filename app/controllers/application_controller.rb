6c12d021c032d29d103967af3b2d4be62cad6332
go-ru <19970929+go-ru@users.noreply.github.com>
2020-09-28 20:54:54 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |id, password|
+      id == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end 
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :furigana_first, :furigana_last, :birthday])
